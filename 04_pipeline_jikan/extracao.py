5c5f524b4dbbc1ec0d444a8d0c0e08d4b57b0bcf
J-Henriquee <josehenriquedoespiritosanto82@gmail.com>
2026-06-24 14:43:11 -0300

    refactor: otimiza funcao de processamento e finaliza pipeline de extracao

--- 04_pipeline_jikan/extracao.py
+++ 04_pipeline_jikan/extracao.py
@@ -1,31 +1,43 @@
 import requests
 import pandas as pd
 from pathlib import Path
-
+import time
 
 arquivo_padrao = Path(__file__).parent
 
-response = requests.get('https://api.jikan.moe/v4/top/anime')
+def processa_dados(response, n):
 
-if response.status_code == 200:
-    print('Sucesso!')
-else:
-    raise Exception(f'Erro {response}')
+    response_json = response.json()
 
+    lista_animes = response_json["data"]
+    dados_limpos = []
 
-response = response.json()
+    for anime in lista_animes:
+        anime_filtrado = {
+            "Titulo": anime["title"],
+            "Score": anime["score"],
+            "Rank": anime["rank"],
+            "Status": anime["status"],
+        }
+        dados_limpos.append(anime_filtrado)
 
-lista_animes = response['data']
-dados_limpos = []
-
-for anime in lista_animes:
-    anime_filtrado = {'Titulo': anime['title'], 'Score': anime['score'], 'Rank': anime['rank'], 'Status': anime['status']}
-    dados_limpos.append(anime_filtrado)
-
-df = pd.DataFrame(dados_limpos)
-
-print(df)
-
-df.to_csv(arquivo_padrao / 'raw_data' / 'top_animes.csv', index=False)
+    df = pd.DataFrame(dados_limpos)
 
 
+    df.to_csv(arquivo_padrao / "raw_data" / f"top_animes_pg{n}.csv", index=False)
+
+
+
+
+
+for n in range(1,41):
+    response = requests.get(f'https://api.jikan.moe/v4/top/anime?page={n}')
+
+    if response.status_code == 200:
+        print("Sucesso!")
+    else:
+        raise Exception(f"Erro {response}")
+
+    processa_dados(response, n)
+
+    time.sleep(2) 
