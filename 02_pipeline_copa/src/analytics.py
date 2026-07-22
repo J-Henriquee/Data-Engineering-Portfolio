fcaace92541ae99d8194a6d00b5f1448261749bc
J-Henriquee <josehenriquedoespiritosanto82@gmail.com>
2026-07-22 18:46:04 -0300

    feat: Normalizando tabelas e criando DW

--- 02_pipeline_copa/src/analytics.py
+++ 02_pipeline_copa/src/analytics.py
@@ -9,7 +9,7 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-
+from transform import transform_data
 load_dotenv()
 
 database_url = os.getenv('DATABASE_URL')
@@ -25,3 +25,27 @@ top_scorers_query = """
 
 top_scorers_df = pd.read_sql_query(top_scorers_query, con=engine)
 print(top_scorers_df)
+
+
+tables_df = transform_data()
+tables_df = tables_df.drop(columns=['gols_visitante', 'gols_casa', 'fase', 'data_utc'])
+tables_df = pd.concat([tables_df['time_visitante'], tables_df['time_casa']], ignore_index=True)
+names_df = pd.DataFrame({'nome_padronizado': tables_df})
+names_df = names_df.drop_duplicates(subset=['nome_padronizado'], keep='first')
+
+names_df.to_sql(    name='dim_selecoes', 
+    con=engine, 
+    if_exists='append', 
+    index=False          
+)
+
+df_dim_selecoes = pd.read_sql_query("SELECT id, nome_padronizado FROM dim_selecoes;", con=engine)
+df_fato_jogos = transform_data()
+fato_df = df_fato_jogos.merge(df_dim_selecoes, left_on='time_casa', right_on='nome_padronizado', how='left')
+fato_df = fato_df.rename(columns={'id': 'id_time_casa'}).drop(columns=['nome_padronizado'])
+fato_df = fato_df.merge(df_dim_selecoes, left_on='time_visitante', right_on='nome_padronizado', how='left')
+fato_df = fato_df.rename(columns={'id': 'id_time_visitante'}).drop(columns=['nome_padronizado'])
+fato_df = fato_df.drop(columns=['time_visitante', 'time_casa'])
+print(fato_df)
+
+
